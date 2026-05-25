cd65964cd13d7099813cd00e87d350024e2a2d78
Rohit <144670577+01rohit01@users.noreply.github.com>
2026-05-25 21:55:53 +0530

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        unordered_set<int> ans;
+        for(int num:nums){
+            ans.insert(num);
+        }
+        return nums.size()>ans.size();
+    //    unordered_set<int> ans;
+    //    for(int num:nums){
+    //     ans.insert(num);
+    //    }
+    //    return nums.size()>ans.size();
+
+         
+    }
+};
