c5e1d72de2ce9a9e85cc5c1e37aa240d7f2dd3f3
Rohit <144670577+01rohit01@users.noreply.github.com>
2026-06-02 14:29:42 +0530

    Add: duplicate-integer - submission-8

---
+++ Data Structures & Algorithms/duplicate-integer/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> ans;
+        for(int num:nums){
+            ans.insert(num);
+        }
+        return ans.size()<nums.size();
+
+    //    unordered_set<int> ans;
+    //    for(int num:nums){
+    //     ans.insert(num);
+    //    }
+    //    return ans.size()<nums.size();
+
+         
+    }
+};
